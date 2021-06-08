76bf6950904b956d13b7b1d7b68be65dfced86a6
ming-gao <59911205+ming-gao@users.noreply.github.com>
2021-06-08 18:10:57 +0800

    github搜索文件归档，antd树和表格组件封装

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react'
-import Search from "./component/Search";
-import List from "./component/List";
+import TableBasic from './component/TableBasic'
+import Tree from './component/Tree'
 class App extends Component {
     // 那里有状态，就在哪里操作状态
-    state={user:[]}
-    setUsers=(user)=>{
-        this.setState({user})
+    state = {column:[]}
+    addColumn=(column)=>{
+        this.setState({column})
     }
     render() {
 
         return (
             <div className="container">
-                <Search setUsers={this.setUsers}/>
-                <List users={this.state.user}/>
+                <TableBasic column={this.state.column}/>
+                <Tree addColumn={this.addColumn}/>
             </div>
         )
     }
---
+++ src/component/Tree/index.jsx
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import { TreeSelect } from 'antd';
+
+class Tree extends Component {
+
+    state = {
+        value: ['0-0-0'],
+    };
+
+    onChange = value => {
+        console.log('onChange ', value);
+        this.setState({ value });
+    };
+    render() {
+        const { SHOW_PARENT } = TreeSelect;
+        const treeData = [
+            {
+                title: 'Node1',
+                value: '0-0',
+                key: '0-0',
+                children: [
+                    {
+                        title: 'Child Node1',
+                        value: '0-0-0',
+                        key: '0-0-0',
+                    },
+                ],
+            },
+            {
+                title: 'Node2',
+                value: '0-1',
+                key: '0-1',
+                children: [
+                    {
+                        title: 'Child Node3',
+                        value: '0-1-0',
+                        key: '0-1-0',
+                    },
+                    {
+                        title: 'Child Node4',
+                        value: '0-1-1',
+                        key: '0-1-1',
+                    },
+                    {
+                        title: 'Child Node5',
+                        value: '0-1-2',
+                        key: '0-1-2',
+                    },
+                ],
+            },
+        ];
+        const tProps = {
+            treeData,
+            value: this.state.value,
+            onChange: this.onChange,
+            treeCheckable: true,
+            showCheckedStrategy: SHOW_PARENT,
+            placeholder: 'Please select',
+            style: {
+                width: '100%',
+            },
+        };
+        return <TreeSelect {...tProps} />;
+    }
+}
+
+export default Tree;
