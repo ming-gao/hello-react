3d51e709f70a56ab43a7bc0a91531e5fff884094
ming-gao <59911205+ming-gao@users.noreply.github.com>
2021-06-08 10:15:47 +0800

    删除操作,全选/反选，一键清除

---
+++ 02_todolist_src/component/List/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import propTypes from 'prop-types'
+import Item from '../Item'
+import './index.css'
+
+export default class List extends Component {
+  // 对接受的props进行限制
+  static propTypes = {
+    todos:propTypes.array.isRequired,
+    updateTodo:propTypes.func.isRequired
+  }
+  render() {
+    const {todos,updateTodo,deleteTodo}=this.props
+    return (
+      <ul className="todo-main">
+        {
+          todos.map((todo)=>{
+            return (<Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo}/>)
+          })
+        }
+      </ul>
+    )
+  }
+}
